52d9f608e73abd3b9b588dfc9ddd9537e6b31bc7
Albert XU <xyb1114@gmail.com>
2019-10-31 18:10:04 +0100

    fixup

--- demo01-formal demo/client/src/index.js
+++ demo01-formal demo/client/src/index.js
@@ -8,8 +8,12 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
 const cache = new InMemoryCache();
+// Specifying the headers option on HttpLink allows us to read the token from localStorage and attach it to the request's headers each time a GraphQL operation is made.
 const link = new HttpLink({
-  uri: 'http://localhost:4000/'
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: localStorage.getItem('token'),
+  }
 });
 
 const client = new ApolloClient({
